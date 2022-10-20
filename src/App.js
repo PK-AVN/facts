1d08d1c6802ce19c9a1de9d8b93ddf78385a985e
Praveen <praveenkumaravn10@gmail.com>
2022-10-20 23:58:20 +0530

    facts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [facts, setFacts] = useState([]);
+  const firstRender = useRef(true);
+
+  const apiCall = async () => {
+    const response = await axios.get('https://catfact.ninja/fact');
+    return response.data;
+  }
+
+  const getFacts = async () => {
+    let promises = [];
+    let currentFact = 0;
+   
+    while (currentFact < 10) {
+      promises.push(apiCall());
+      currentFact++;
+    }
+
+    if (firstRender.current) {
+      firstRender.current = false;
+    }
+
+    try {
+      const factsss = await Promise.all(promises);
+      console.log(factsss, "factsss")
+      factsss.forEach((each) => {
+        setFacts((prev) => ([...prev, each.fact]))
+      })
+    }
+    catch {
+      throw Error("Promise failed");
+    }
+  }
+
+  useEffect(() => {
+    setFacts([]);
+    getFacts();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {facts.map((fact, index) => <li key={index}>{fact}</li>)}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// useEffect(() => {
+//   const apiCall = ()=> {
+//     fetch('https://catfact.ninja/fact ')
+//       .then(response => response.json())
+//       .then(jsonResponse => {
+//         console.log(jsonResponse.fact);
+//         setFacts((prev) => ([...prev, jsonResponse.fact
+//         ]))
+//       });
+//   }
+//   for (let index = 0; index < 5; index++) {
+//     apiCall();
+//   }
+// }, []);
